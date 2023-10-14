0fea35f68d8944964949f7e5476165a1cb68f511
codeph-0bia <codephobia642@gmail.com>
2023-10-14 21:36:01 +0530

    updated button for offer

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -14,19 +14,19 @@ const SpecialOffer = () => {
         />
 		  </div>
 		  <div className="flex flex-1 flex-col">
-			  <h2>
-				<span>Special </span> Offer
+			  <h2 className='text-4xl font-palanquin font-bold'>
+				<span className='text-coral-red'>Special </span> Offer
 			  </h2>
-			  <p>
+			  <p className='mt-4 info-text'>
 				Embark on a shopping journey that redefines your experience with
           		unbeatable deals. From premier selections to incredible savings, we
 				offer unparalleled value that sets us apart.</p>
-			  <p>
+			  <p className='mt-6 info-text'>
 				Navigate a realm of possibilities designed to fulfill your unique
           		desires, surpassing the loftiest expectations. Your journey with us is
           		nothing short of exceptional.
 			  </p>
-			  <div className="">
+			  <div className="mt-11 flex flex-wrap gap-6;">
 				  <Button label='Shop Now' iconUrl={arrowRight} />
 				  <Button label='Learn More' backgroundColor='bg-white' borderColor='border-slate-gray'
 				  	textColor='text-slate-gray'
