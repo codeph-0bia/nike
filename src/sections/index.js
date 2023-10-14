bc461e2d2e6e88dbedd11c89bbe6c4c86c6aa341
codeph-0bia <codephobia642@gmail.com>
2023-10-14 16:58:06 +0530

    pages setup ✅

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from './Hero';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import SuperQuality from "./SuperQuality";
+import Subscribe from './Subscribe';
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+
+}
