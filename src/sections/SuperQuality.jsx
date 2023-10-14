5889cd0d18fd74b7f135548c8fdcb2319aac06ac
codeph-0bia <codephobia642@gmail.com>
2023-10-14 20:52:51 +0530

    super quality section init

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,6 +1,40 @@
+import { shoe8 } from "../assets/images"
+import { Button } from "../components"
+
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container "
+    >
+      <div className="flex flex-1 flex-col">
+        <h2>
+          We Provide You
+          <span>Super</span>
+          <span>Quality </span> Shoes
+        </h2>
+        <p>
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p>
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className="mt-11">
+          <Button label='View Details'/>
+        </div>
+
+        {/*image*/}
+
+        <div className="flex">
+          <img src={shoe8} alt="product detail" width={570} height={522} className="object-contain" />
+        </div>
+
+
+      </div>
+
+    </section>
   )
 }
 
