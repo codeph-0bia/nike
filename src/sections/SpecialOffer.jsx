37c29c122d6e497f59fe0867ac4a07e3465576fc
codeph-0bia <codephobia642@gmail.com>
2023-10-14 21:31:17 +0530

    special offer text

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -12,9 +12,30 @@ const SpecialOffer = () => {
           height={687}
           className='object-contain w-full'
         />
-      </div>
+		  </div>
+		  <div className="flex flex-1 flex-col">
+			  <h2>
+				<span>Special </span> Offer
+			  </h2>
+			  <p>
+				Embark on a shopping journey that redefines your experience with
+          		unbeatable deals. From premier selections to incredible savings, we
+				offer unparalleled value that sets us apart.</p>
+			  <p>
+				Navigate a realm of possibilities designed to fulfill your unique
+          		desires, surpassing the loftiest expectations. Your journey with us is
+          		nothing short of exceptional.
+			  </p>
+			  <div className="">
+				  <Button label='Shop Now' iconUrl={arrowRight} />
+				  <Button label='Learn More' backgroundColor='bg-white' borderColor='border-slate-gray'
+				  	textColor='text-slate-gray'
+				  />
+				</div>
 
-      
+		  </div>
+
+
     </section>
   )
 }
