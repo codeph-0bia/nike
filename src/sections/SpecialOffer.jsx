28310bc2f4d7d85fd3ce4550dc409a37fd790118
codeph-0bia <codephobia642@gmail.com>
2023-10-14 21:25:56 +0530

    special offer img

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,7 +1,21 @@
+import { arrowRight } from '../assets/icons'
+import { offer } from '../assets/images'
+import { Button } from '../components'
 
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className='flex justify-between items-center max-xl:flex-col-reverse gap-10 max-container'>
+      <div className="flex-1">
+        <img src={offer}
+          alt="Shoe promotion"
+          width={773}
+          height={687}
+          className='object-contain w-full'
+        />
+      </div>
+
+      
+    </section>
   )
 }
 
