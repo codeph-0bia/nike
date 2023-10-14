87123c3ee7575b3c0e25157a306d4b418587d11a
codeph-0bia <codephobia642@gmail.com>
2023-10-14 19:40:57 +0530

    hero text ✅

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -14,14 +14,14 @@ const Hero = () => {
           <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
           
         </h1>
-        <p>Discover stylish Nike arrivals, quality  comfort, and innovaton for your active life. </p>
+        <p className='font-montserrat text-slate-gray text-lg leading-8  mt-6 mb-14 sm:max-w-sm '>Discover stylish Nike arrivals, quality  comfort, and innovaton for your active life. </p>
         <Button label="Shop Now" iconUrl={arrowRight} />
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
           {
             statistics.map((stat) => (
               <div key={stat.label}>
-                <p>{stat.value}</p>
-                <p>{stat.label}</p>
+                <p className='text-4xl font-palanquin font-bold'>{stat.value}</p>
+                <p className='font-montserrat leading-7 text-slate-gray'>{stat.label}</p>
               </div>
             ))
           }
