ecb17c0ee56717ca44a80ba8bfde6afb159a6b91
codeph-0bia <codephobia642@gmail.com>
2023-10-14 20:59:27 +0530

    superQuality section ✅

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -8,26 +8,25 @@ const SuperQuality = () => {
       className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container "
     >
       <div className="flex flex-1 flex-col">
-        <h2>
+        <h2 className="font-palanquin capitalize text-4xl lg:max-w-lg font-bold">
           We Provide You
-          <span>Super</span>
-          <span>Quality </span> Shoes
+          <span className="text-coral-red"> Super</span>
+          <span className="text-coral-red"> Quality </span> Shoes
         </h2>
-        <p>
+        <p className="mt-4 lg:max-w-lg info-text">
           Ensuring premium comfort and style, our meticulously crafted footwear
           is designed to elevate your experience, providing you with unmatched
           quality, innovation, and a touch of elegance.
         </p>
-        <p>
+        <p className="mt-6 lg:max-w-lg info-text">
           Our dedication to detail and excellence ensures your satisfaction
         </p>
         <div className="mt-11">
           <Button label='View Details'/>
         </div>
 
-        {/*image*/}
 
-        <div className="flex">
+        <div className="flex flex-1 justify-center items">
           <img src={shoe8} alt="product detail" width={570} height={522} className="object-contain" />
         </div>
 
